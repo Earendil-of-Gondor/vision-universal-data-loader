1a2893a194eaed64f0bbc1cfa054f2901888afac
Edward Liujie Zheng <liujiez@email.unc.edu>
2023-04-27 15:28:20 -0400

    update

--- video2images.py
+++ video2images.py
@@ -10,9 +10,11 @@ SCRIPTS_FOLDER = os.path.join(ROOT_DIR, "scripts")
 
 def parse_args():
 	parser = argparse.ArgumentParser(description="Convert a text colmap export to nerf format transforms.json; optionally convert video to images, and optionally run colmap in the first place.")
-
 	parser.add_argument("--video_in", default="", help="Run ffmpeg first to convert a provided video file into a set of images. Uses the video_fps parameter also.")
 	parser.add_argument("--video_fps", default=2)
+	parser.add_argument("--images", default="images", help="Input path to the images.")
+	parser.add_argument("--overwrite", action="store_true", help="Do not ask for confirmation for overwriting existing images and COLMAP data.")
+	parser.add_argument("--time_slice", default="", help="Time (in seconds) in the format t1,t2 within which the images should be generated from the video. E.g.: \"--time_slice '10,300'\" will generate images only from 10th second to 300th second of the video.")
 	args = parser.parse_args()
 	return args
 
@@ -63,7 +65,7 @@ def run_ffmpeg(args):
 		time_slice_value = f",select='between(t\,{start}\,{end})'"
 	do_system(f"{ffmpeg_binary} -i {video} -qscale:v 1 -qmin 1 -vf \"fps={fps}{time_slice_value}\" {images}/%04d.jpg")
 
-	if __name__ == "__main__":
-		args = parse_args()
-		if args.video_in != "":
-			run_ffmpeg(args)
+if __name__ == "__main__":
+	args = parse_args()
+	if args.video_in != "":
+		run_ffmpeg(args)
