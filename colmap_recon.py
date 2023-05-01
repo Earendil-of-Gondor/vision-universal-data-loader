64ed11c15681989b58617612584f1435995962e0
Earendil-of-Gondor <wzh@unc.edu>
2023-04-30 20:44:35 -0500

    enabled colmap gpu

---
+++ colmap_recon.py
@@ -0,0 +1,150 @@
+import os
+import subprocess
+
+
+# $ DATASET_PATH=/path/to/dataset
+
+# $ colmap feature_extractor \
+#    --database_path $DATASET_PATH/database.db \
+#    --image_path $DATASET_PATH/images
+
+# $ colmap exhaustive_matcher \
+#    --database_path $DATASET_PATH/database.db
+
+# $ mkdir $DATASET_PATH/sparse
+
+# $ colmap mapper \
+#     --database_path $DATASET_PATH/database.db \
+#     --image_path $DATASET_PATH/images \
+#     --output_path $DATASET_PATH/sparse
+
+# $ mkdir $DATASET_PATH/dense
+def run_colmap(basedir, match_type, run_until=6):
+    '''
+    run_until specifies where to stop colmap at. 1 for feature extractor, 2 for matcher,
+    3 for sparse mapper, 4 for undistorter, 5 for patch matcher, 6 for fusion, 7 for poisson mesher, 
+    8 for delaunay mesher
+    '''
+    logfile_name = os.path.join(basedir, 'colmap_output.txt')
+    logfile = open(logfile_name, 'w')
+
+    feature_extractor_args = [
+        'colmap', 'feature_extractor',
+        '--database_path', os.path.join(basedir, 'database.db'),
+        '--image_path', os.path.join(basedir, 'images'),
+        '--ImageReader.single_camera', '1',
+        # '--SiftExtraction.use_gpu', '0',
+    ]
+    feat_output = (subprocess.check_output(
+        feature_extractor_args, universal_newlines=True))
+    logfile.write(feat_output)
+    print('Features extracted')
+
+    if run_until >= 2:
+        exhaustive_matcher_args = [
+            'colmap', match_type,
+            '--database_path', os.path.join(basedir, 'database.db'),
+            # '--SiftMatching.use_gpu', '0'
+        ]
+
+        match_output = (subprocess.check_output(
+            exhaustive_matcher_args, universal_newlines=True))
+        logfile.write(match_output)
+        print('Features matched')
+
+    if run_until >= 3:
+        p = os.path.join(basedir, 'sparse')
+        if not os.path.exists(p):
+            os.makedirs(p)
+
+        mapper_args = [
+            'colmap', 'mapper',
+            '--database_path', os.path.join(basedir, 'database.db'),
+            '--image_path', os.path.join(basedir, 'images'),
+            # --export_path changed to --output_path in colmap 3.6
+            '--output_path', os.path.join(basedir, 'sparse'),
+            '--Mapper.num_threads', '16',
+            '--Mapper.init_min_tri_angle', '4',
+            '--Mapper.multiple_models', '0',
+            '--Mapper.extract_colors', '0',
+        ]
+
+        map_output = (subprocess.check_output(
+            mapper_args, universal_newlines=True))
+        logfile.write(map_output)
+        print('Sparse map created')
+
+    if run_until >= 4:
+        p = os.path.join(basedir, 'dense')
+        if not os.path.exists(p):
+            os.makedirs(p)
+
+        undistorter_args = [
+            'colmap', 'image_undistorter',
+            '--input_path', os.path.join(basedir, 'sparse', '0'),
+            '--image_path', os.path.join(basedir, 'images'),
+            '--output_path', os.path.join(basedir, 'dense'),
+            '--output_type', 'COLMAP',
+            '--max_image_size', '2000',
+        ]
+
+        undistorter_output = (subprocess.check_output(
+            undistorter_args, universal_newlines=True))
+        logfile.write(undistorter_output)
+        print('Undistorted images created')
+
+    if run_until >= 5:
+        stereo_matcher_args = [
+            'colmap', 'patch_match_stereo',
+            '--workspace_path', os.path.join(basedir, 'dense'),
+            '--workspace_format', 'COLMAP',
+            '--PatchMatchStereo.geom_consistency', 'true',
+        ]
+
+        stereo_matcher_output = (subprocess.check_output(
+            stereo_matcher_args, universal_newlines=True))
+        logfile.write(stereo_matcher_output)
+        print('stereo matched')
+
+    if run_until >= 6:
+        fusion_args = [
+            'colmap', 'stereo_fusion',
+            '--workspace_path', os.path.join(basedir, 'dense'),
+            '--workspace_format', 'COLMAP',
+            '--output_path', os.path.join(basedir, 'dense', 'fused.ply'),
+            '--input_type', 'geometric',
+        ]
+
+        fusion_output = (subprocess.check_output(
+            fusion_args, universal_newlines=True))
+        logfile.write(fusion_output)
+        print('fused')
+
+    if run_until >= 7:
+        poisson_args = [
+            'colmap', 'poisson_mesher',
+            '--input_path', os.path.join(basedir, 'dense', 'fused.ply'),
+            '--output_path', os.path.join(basedir,
+                                          'dense', 'meshed-poisson.ply'),
+        ]
+
+        poisson_output = (subprocess.check_output(
+            poisson_args, universal_newlines=True))
+        logfile.write(poisson_output)
+        print('poisson mesh created')
+
+    if run_until >= 8:
+        delaunay_args = [
+            'colmap', 'delaunay_mesher',
+            '--input_path', os.path.join(basedir, 'dense'),
+            '--output_path', os.path.join(basedir,
+                                          'dense', 'meshed-delaunay.ply'),
+        ]
+
+        delaunay_output = (subprocess.check_output(
+            delaunay_args, universal_newlines=True))
+        logfile.write(delaunay_output)
+        print('delaunay mesh created')
+
+    logfile.close()
+    print('Finished running COLMAP, see {} for logs'.format(logfile_name))
--- llff/poses/colmap_wrapper.py
+++ llff/poses/colmap_wrapper.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 
 
-
 # $ DATASET_PATH=/path/to/dataset
 
 # $ colmap feature_extractor \
@@ -21,38 +20,40 @@ import subprocess
 
 # $ mkdir $DATASET_PATH/dense
 def run_colmap(basedir, match_type):
-    
+
     logfile_name = os.path.join(basedir, 'colmap_output.txt')
     logfile = open(logfile_name, 'w')
 
     feature_extractor_args = [
         'colmap', 'feature_extractor',
-            '--database_path', os.path.join(basedir, 'database.db'),
-            '--image_path', os.path.join(basedir, 'images'),
-            '--ImageReader.single_camera', '1',
-            '--SiftExtraction.use_gpu', '0',
+        '--database_path', os.path.join(basedir, 'database.db'),
+        '--image_path', os.path.join(basedir, 'images'),
+        '--ImageReader.single_camera', '1',
+        # '--SiftExtraction.use_gpu', '0',
     ]
-    feat_output = ( subprocess.check_output(feature_extractor_args, universal_newlines=True) )
+    feat_output = (subprocess.check_output(
+        feature_extractor_args, universal_newlines=True))
     logfile.write(feat_output)
     print('Features extracted')
 
     exhaustive_matcher_args = [
         'colmap', match_type,
-            '--database_path', os.path.join(basedir, 'database.db'),
-        '--SiftMatching.use_gpu', '0'
+        '--database_path', os.path.join(basedir, 'database.db'),
+        # '--SiftMatching.use_gpu', '0'
     ]
 
-    match_output = ( subprocess.check_output(exhaustive_matcher_args, universal_newlines=True) )
+    match_output = (subprocess.check_output(
+        exhaustive_matcher_args, universal_newlines=True))
     logfile.write(match_output)
     print('Features matched')
-    
+
     p = os.path.join(basedir, 'sparse')
     if not os.path.exists(p):
         os.makedirs(p)
 
     # mapper_args = [
-    #     'colmap', 'mapper', 
-    #         '--database_path', os.path.join(basedir, 'database.db'), 
+    #     'colmap', 'mapper',
+    #         '--database_path', os.path.join(basedir, 'database.db'),
     #         '--image_path', os.path.join(basedir, 'images'),
     #         '--output_path', os.path.join(basedir, 'sparse'),
     #         '--Mapper.num_threads', '16',
@@ -60,20 +61,20 @@ def run_colmap(basedir, match_type):
     # ]
     mapper_args = [
         'colmap', 'mapper',
-            '--database_path', os.path.join(basedir, 'database.db'),
-            '--image_path', os.path.join(basedir, 'images'),
-            '--export_path', os.path.join(basedir, 'sparse'), # --export_path changed to --output_path in colmap 3.6
-            '--Mapper.num_threads', '16',
-            '--Mapper.init_min_tri_angle', '4',
-            '--Mapper.multiple_models', '0',
-            '--Mapper.extract_colors', '0',
+        '--database_path', os.path.join(basedir, 'database.db'),
+        '--image_path', os.path.join(basedir, 'images'),
+        # --export_path changed to --output_path in colmap 3.6
+        '--output_path', os.path.join(basedir, 'sparse'),
+        '--Mapper.num_threads', '16',
+        '--Mapper.init_min_tri_angle', '4',
+        '--Mapper.multiple_models', '0',
+        '--Mapper.extract_colors', '0',
     ]
 
-    map_output = ( subprocess.check_output(mapper_args, universal_newlines=True) )
+    map_output = (subprocess.check_output(
+        mapper_args, universal_newlines=True))
     logfile.write(map_output)
     logfile.close()
     print('Sparse map created')
-    
-    print( 'Finished running COLMAP, see {} for logs'.format(logfile_name) )
 
-
+    print('Finished running COLMAP, see {} for logs'.format(logfile_name))
