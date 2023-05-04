a07836320cce5fa7467225e608f494b6ade12086
Earendil-of-Gondor <wzh@unc.edu>
2023-05-04 12:02:11 -0500

    fix

--- resize.sh
+++ resize.sh
@@ -1,19 +1,19 @@
 
 
-cp -r "$DATASET_PATH"/images "$DATASET_PATH"/images_2
+cp -r "$DATASET_PATH" "$DATASET_PATH"_2
 
-pushd "$DATASET_PATH"/images_2
+pushd "$DATASET_PATH"_2
 ls | xargs -P 8 -I {} mogrify -resize 50% {}
 popd
 
-cp -r "$DATASET_PATH"/images "$DATASET_PATH"/images_4
+cp -r "$DATASET_PATH" "$DATASET_PATH"_4
 
-pushd "$DATASET_PATH"/images_4
+pushd "$DATASET_PATH"_4
 ls | xargs -P 8 -I {} mogrify -resize 25% {}
 popd
 
-cp -r "$DATASET_PATH"/images "$DATASET_PATH"/images_8
+cp -r "$DATASET_PATH" "$DATASET_PATH"_8
 
-pushd "$DATASET_PATH"/images_8
+pushd "$DATASET_PATH"_8
 ls | xargs -P 8 -I {} mogrify -resize 12.5% {}
 popd
